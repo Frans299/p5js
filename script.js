0875d4c490b45394661f98f322294365f3eabeb9
Frans Vaes <78485938+Frans299@users.noreply.github.com>
2021-09-08 10:52:24 +0000

    iets

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 var x, y;
 
 function setup() {
-	createCanvas(500, 400);
+	createCanvas(400, 400);
 
   x = 0;
   y = 200;
-  v = 5;
+  vx = 5;
+  vy = 5;
 }
 
 function draw() {
 	background(250,0,0);
   
   ellipse(x,y,50,50);
-  x = x + v;
+  x = x + vy;
+  y = y + vx;
 
-  if(x <= 0 || x > 500){
-    v = v * -1;
+  if(x <= 0 || x > 400){
+    vx = vx * -1;
+  }
+
+  if(y <= 0 || y > 400){
+    vy = vy * -1;
   }
 
 }
