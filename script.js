0c0f850b1d45c5cb95b28b465217fc9df491e0ea
Frans Vaes <78485938+Frans299@users.noreply.github.com>
2021-09-15 10:51:12 +0000

    nog iets

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 class ball {
-  constructor(x, y, h, w, vx, vy){
+  constructor(x, y, h, w, vx, vy) {
     this.x = x;
     this.y = y;
     this.w = w;
@@ -7,32 +7,39 @@ class ball {
     this.vx = vx;
     this.vy = vy;
   }
+
+  drawball(){
+    ellipse(this.x, this.y, this.w, this.y);
+    this.x = this.x + this.x;
+    this.y = this.y + this.y;
+
+    if (this.x <= 0 || this.x > 500) {
+      this.vx = this.vx * -1;
+    }
+
+    if (this.y <= 0 || this.y > 400) {
+      this.vy = this.vy * -1;
+    }
+  }
 }
 
-drawball(){
-  ellipse(this.x, this.y, this.w, this.y);
-  this.x = this.x + this.x;
-  this.y = this.y + this.y;
 
-  if(this.x <= 0 || this.x > 500){
-    this.vx = this.vx * -1;
-  }
-
-  if(this.y <= 0 || this.y > 400){
-    this.vy = this.vy * -1;
-  }
-}
 
 function setup() {
-	createCanvas(500, 400);
-  background(250,0,0);
+  createCanvas(500, 400);
+  background(250, 0, 0);
 
   ball1 = new ball(0, 300, 50, 50, 5, 5)
 }
 
+function draw(){
+  background(250, 0, 0);
+
+  ball1.drawball();
+}
 
 
 
 
 
- 
+
